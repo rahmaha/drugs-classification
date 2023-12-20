03b86dabb4ccfa4e0bd9dca805cff6af2faab5c7
rahmaha <rahma.hayuning712@gmail.com>
2023-12-21 04:51:30 +0700

    edit some files

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import requests
 
 # url = "http://localhost:9696/predict"  # to run locally using docker
-host = "https://drugs-classifications-afaf29e1854b.herokuapp.com/"
-# host = "drugs-classification-capstone-env.eba-c9pvy2sr.ap-southeast-1.elasticbeanstalk.com"
+host = "coba-capstone1-env.eba-kftbizs5.ap-southeast-1.elasticbeanstalk.com."
 url =  f"http://{host}/predict"
 
 patient = {
